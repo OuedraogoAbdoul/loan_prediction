68f5a8fa2076e1a0dc69e6508e9943515547b39c
Abdoul Ouedraogo <ouabdoul@hotmail.com>
2022-02-09 10:43:39 -0800

    feat: Complete download_data function

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,4 +1,35 @@
-# download or generate data
+# # download or generate data
+import pandas as pd
+import argparse
+import yaml
+import os
+
+
+
+def get_main_directory_path():
+    return os.path.dirname(__file__).split("src")[0]
+
+
+
+def read_params_file(config_path):
+
+    with open(config_path) as yaml_file:
+        config = yaml.safe_load(yaml_file)
+
+    return config
+
+
+
+def download_data(config_path):
+    data_path = read_params_file(config_path)
+    df_data = pd.read_csv(data_path["load_data"]["row_dataset_link"])
+
+    df_data.to_csv(os.path.join(get_main_directory_path(), data_path["load_data"]["raw_dataset_csv"]))
+
+
+
+
+
 
 def load_datasets():
     pass
@@ -7,3 +38,18 @@ def load_datasets():
 
 def split_datasets():
     pass
+
+
+    
+
+
+
+if __name__ =='__main__':
+    config_yml_path = os.path.join(get_main_directory_path(), "config/params.yaml") 
+    
+    parser = argparse.ArgumentParser(description="Take data path")
+    parser.add_argument("--config", default=config_yml_path)
+    parse_args = parser.parse_args()
+
+    data = download_data(config_path=config_yml_path)
+    
