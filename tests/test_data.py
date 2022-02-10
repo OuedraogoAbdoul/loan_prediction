1102a1cb6e0909a147c1097a9b472edebf9805c0
Abdoul Ouedraogo <ouabdoul@hotmail.com>
2022-02-09 16:32:40 -0800

    feat: Update test case for data presence

--- tests/test_data.py
+++ tests/test_data.py
@@ -8,11 +8,12 @@ import pytest
 
 
 param_path = os.path.join(os.path.dirname(cf.__file__), "params.yaml")
-config = read_params_file(param_path)
+param_path = os.path.join(os.path.dirname(cf.__file__), "raw_data.csv")
+# config = read_params_file(param_path)
 
-data_path = os.path.join(os.path.dirname(raw.__file__), config["load_data"]["raw_dataset_csv"])
+# data_path = os.path.join(os.path.dirname(raw.__file__), config["load_data"]["raw_dataset_csv"])
 
 
 def test_data_exist():
     
-    assert(exists(data_path) == True, "Passed test")
+    assert(exists(param_path) == True, "Passed test")
